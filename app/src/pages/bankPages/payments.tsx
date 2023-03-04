dca9dd1c9efefd2e8987ec174ce441b0ce1b0707
charliewager <charliewager@uvic.ca>
2023-03-03 22:33:10 -0800

    added pages for accounts, budgets,ts, and payments

--- app/src/pages/bankPages/payments.tsx
+++ app/src/pages/bankPages/payments.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import styles from "@/styles/Home.module.css"
-import { useAuth } from "../context/UserContext"
+import { useAuth } from "../../context/UserContext"
 import { Button } from '@mui/material';
 import { useRouter } from "next/router"
 
@@ -17,22 +17,16 @@ export default function Main() {
     <>
       <Head>
         <title>Piggy Bank</title>
-        <meta name="description" content="Piggy Bank Home Page" />
+        <meta name="description" content="Piggy Bank bank account" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/piggy-bank.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>Welcome to PiggyBank {user?.username || ""}</div>
+        <div className={styles.description}>{user?.username || ""}, here are your recent and reccurring payments</div>
         <div>
           <Button variant="contained" onClick={() => {
-            router.push("/bankPages/accounts");
-          }}>Accounts</Button>
-          <Button variant="contained" onClick={() => {
-            router.push("/bankPages/budgets");
-          }}>Budgets</Button>
-          <Button variant="contained" onClick={() => {
-            router.push("/bankPages/payments");
-          }}>Payments</Button>
+            router.push("/main")
+          }}>Back</Button>
         </div>
       </main>
     </>
