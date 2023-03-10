7e3da8b92e8829f36398edf442d84d8a51b3a807
Ewan Morgan <emorg19@uvic.ca>
2023-03-10 13:14:12 -0800

    updating HP

--- app/src/components/footer.tsx
+++ app/src/components/footer.tsx
@@ -16,8 +16,8 @@ const Footer: NextPage = () => {
   return (
     <Box p={4} sx={useStyles.footer}>
       Created by: Group #13 | Kjartan Einarsson V00885049 | Amy Finck V00878512
-      | Charlie Wager V00959352 | Connor Newbery V00921506 | Connor Newbery
-      V00921506
+      | Charlie Wager V00959352 | Connor Newbery V00921506 | Ewan Morgan
+      V00948587
     </Box>
   )
 }
